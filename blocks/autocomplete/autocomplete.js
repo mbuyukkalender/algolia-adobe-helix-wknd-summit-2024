49788b6318d308c8a8d9e7e420d0054ed7115af7
Massis Buyukkalender <massis.buyukkalender@algolia.com>
2024-01-25 15:23:20 +0100

    test

--- blocks/autocomplete/autocomplete.js
+++ blocks/autocomplete/autocomplete.js
@@ -55,16 +55,12 @@ export default function decorate(block) {
               templates: {
                 item({ item, components, html }) {
                   return html`
-                    <div style="cursor: pointer; padding-top: 0.75rem; padding-bottom: 0.75rem;">
+                    
                       <div style="display: flex; align-items: center;">
                         <div style="position: relative; margin-right: 1rem; width: 5rem; flex-shrink: 0; align-self: center;">
                           <img style="aspect-ratio: 1 / 1; width: 100%; object-fit: contain;" src="${item.image_url}" alt="${item.name}">
                         </div>
                         
-<div style="position: relative; margin-right: 1rem; width: 5rem; flex-shrink: 0; align-self: center;">
-                          <img style="aspect-ratio: 1 / 1; width: 100%; object-fit: contain;" src="${item.image_url}" alt="${item.name}">
-                        </div>
-
                         <div style="position: relative; align-self: center;">
                           <p style="margin-bottom: 0.25rem; font-size: .75rem; font-weight: 700; text-transform: uppercase; line-height: 1;">
                             ${components.Highlight({
@@ -84,7 +80,7 @@ export default function decorate(block) {
                         </div>
 
                       </div>
-                    </div>
+                    
                   `;
                 },
               },
