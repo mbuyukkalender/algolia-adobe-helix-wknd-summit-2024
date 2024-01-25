0d162ba56903366dcf437221797df364b9193b98
Massis Buyukkalender <massis.buyukkalender@algolia.com>
2024-01-25 15:07:55 +0100

    test

--- blocks/autocomplete/autocomplete.js
+++ blocks/autocomplete/autocomplete.js
@@ -56,7 +56,14 @@ export default function decorate(block) {
                 item({ item, components, html }) {
                   return html`
                     <div style="cursor: pointer; padding-top: 0.75rem; padding-bottom: 0.75rem;">
-                      
+                      <div style="display: flex; align-items: center;">
+                        <div style="position: relative; margin-right: 1rem; width: 5rem; flex-shrink: 0; align-self: center;">
+                          <img style="aspect-ratio: 1 / 1; width: 100%; object-fit: contain;" src="${item.image_url}" alt="${item.name}">
+                        </div>
+                        
+                        
+
+                      </div>
                     </div>
                   `;
                 },
