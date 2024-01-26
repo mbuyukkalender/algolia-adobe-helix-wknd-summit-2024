42e631906b40d909c4462b9e60cffa7833009026
Massis Buyukkalender <massis.buyukkalender@algolia.com>
2024-01-26 15:06:59 +0100

    query suggestions -> setisopen

--- blocks/autocomplete/autocomplete.js
+++ blocks/autocomplete/autocomplete.js
@@ -34,6 +34,14 @@ export default function decorate(block) {
             hitsPerPage: 7,
           };
         },
+        transformSource({ source, onTapAhead }) {
+          return {
+            ...source,
+            onSelect({ setIsOpen }) {
+              setIsOpen(true);
+            },
+          };
+        },
       });
 
       autocomplete({
