34c4f41d9d06c059a1cac7b1a6d0f18a3e36eb1d
Massis Buyukkalender <massis.buyukkalender@algolia.com>
2024-01-31 16:10:47 +0100

    test

--- blocks/autocomplete/autocomplete.js
+++ blocks/autocomplete/autocomplete.js
@@ -63,36 +63,9 @@ export default function decorate(block) {
         recommendClient,
         indexName,
         itemComponent({ item }) {
-          return html`
-            <a
-              href="${item.url}"
-              target="_self"
-              rel="noreferrer noopener"
-              className="aa-ItemLink aa-ProductItem"
-              style="text-decoration: none;"
-            >
-              <div style="cursor: pointer; padding-top: 0.75rem; padding-bottom: 0.75rem;">
-                <div style="display: flex; align-items: center;">
-                  <div style="position: relative; margin-right: 1rem; width: 5rem; flex-shrink: 0; align-self: center;">
-                    <img style="aspect-ratio: 1 / 1; width: 100%; object-fit: contain;" src="${item.image_url}" alt="${item.name}"/>
-                  </div>
-                  
-                  <div style="position: relative; align-self: center;">
-                    <p style="margin-bottom: 0.25rem; font-size: .75rem; font-weight: 700; text-transform: uppercase; line-height: 1;">
-                      ${components.Highlight({
-                        hit: item,
-                        attribute: 'name',
-                      })}
-                    </p>
-                    <p style="font-size: .75rem; line-height: 1rem; font-weight: 700; color: #003DFF; ">
-                      <span>${item.price.USD.default_formated}</span>
-                    </p>
-                  </div>
-
-                </div>
-              </div>
-            </a>
-          `;
+          return html`<pre>
+      <code>${JSON.stringify(item)}</code>
+    </pre>`;
         },
       });
 
