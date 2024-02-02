ae5da84bb6e765e79a1c27a13e249a6474112bca
Massis Buyukkalender <massis.buyukkalender@algolia.com>
2024-02-02 15:15:04 +0100

    test

--- blocks/instant-search/instant-search.js
+++ blocks/instant-search/instant-search.js
@@ -33,8 +33,8 @@ export default function decorate(block) {
     <div id="searchbox" style="width:100%"></div>
 
     <div class="tab">
-      <button class="tablinks" onclick="${onCtaCoverClick(this,"All")}">ALL RESULTS</button>
-      <button class="tablinks" onclick="${onCtaCoverClick(this,"Products")}">PRODUCTS</button>
+      <button class="tablinks" onclick="${openTab(this,"All")}">ALL RESULTS</button>
+      <button class="tablinks" onclick="${openTab(this,"Products")}">PRODUCTS</button>
       <button class="tablinks" onclick="openTab(event, 'Articles')">ARTICLES</button>
     </div>
 
