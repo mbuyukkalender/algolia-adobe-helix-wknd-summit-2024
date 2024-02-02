804108c46616b190b417d53ca55e96a9d1b05039
Massis Buyukkalender <massis.buyukkalender@algolia.com>
2024-02-02 17:02:29 +0100

    testr

--- blocks/instant-search/instant-search.js
+++ blocks/instant-search/instant-search.js
@@ -1,7 +1,17 @@
 import '../../scripts/lib-algoliasearch.js';
 import '../../scripts/lib-instant-search.js';
 
-  /*const openTab = (evt, tabName) => {
+export default function decorate(block) {
+  const { algoliasearch, instantsearch } = window;
+  const {
+    searchBox, stats, hits, configure, panel, refinementList, pagination, rangeSlider
+  } = instantsearch.widgets;
+  const params = new URL(document.location).searchParams;
+  const query = params.get('query');
+
+  const { connectHits } = instantsearch.connectors;
+
+  const openTab = (evt, tabName) => {
     alert("haha");
     let tabcontent = document.getElementsByClassName("tabcontent");
 
@@ -21,28 +31,19 @@ import '../../scripts/lib-instant-search.js';
     
     if (evt != null) {
       evt.currentTarget.className += " active";
-    }
-  };*/
+    }*/
+  };
 
- function openTab2(){
+ const openTab2 = () => {
     alert("hehe");
-  }
+  };
 
-export default function decorate(block) {
-  const { algoliasearch, instantsearch } = window;
-  const {
-    searchBox, stats, hits, configure, panel, refinementList, pagination, rangeSlider
-  } = instantsearch.widgets;
-  const params = new URL(document.location).searchParams;
-  const query = params.get('query');
-
-  const { connectHits } = instantsearch.connectors;
 
   block.innerHTML = `
     <div id="searchbox" style="width:100%"></div>
 
     <div class="tab">
-      <button class="tablinks" onClick="openTab2()">ALL RESULTS</button>
+      <button class="tablinks" onClick=${openTab2}>ALL RESULTS</button>
     </div>
 
 
