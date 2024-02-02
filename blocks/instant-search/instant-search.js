64ec3382adb9dea62fd03ff81fdf7c010e225f23
Massis Buyukkalender <massis.buyukkalender@algolia.com>
2024-02-02 16:23:54 +0100

    test

--- blocks/instant-search/instant-search.js
+++ blocks/instant-search/instant-search.js
@@ -38,7 +38,7 @@ export default function decorate(block) {
     <div id="searchbox" style="width:100%"></div>
 
     <div class="tab">
-      <button class="tablinks" onClick=${openTab}>ALL RESULTS</button>
+      <button class="tablinks" onClick=${openTab(this,"All")}>ALL RESULTS</button>
       <button class="tablinks" onClick=${openTab(this,"Products")}>PRODUCTS</button>
       <button class="tablinks" onClick="alert(this)">ARTICLES</button>
     </div>
